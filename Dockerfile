0cd4861afedab578826c3202ddbe8df8c44f568f
GPereira2609 <gabriel.pereira2609@gmail.com>
2023-04-03 20:26:06 -0300

    correção do docker compose

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM python:3
-
-WORKDIR /socket
-
-COPY . .
-
-CMD ["python", "socket/server.py"]
---
+++ client/client.py
@@ -0,0 +1,54 @@
+import threading
+import socket
+
+host = 'servidor'
+port = 5000
+
+
+def main():
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        client.connect((host, port))
+    except:
+        return print('\nA conexão não pôde ser estabelecida\n')
+
+    print(f"\nConexão estabelecida\nHost = {host}\nPorta = {port}")
+
+    thread1 = threading.Thread(target=receber_mensagem, args=[client])
+    thread2 = threading.Thread(target=enviar_mensagem, args=[client])
+
+    thread1.start()
+    thread2.start()
+
+
+def receber_mensagem(client):
+    while True:
+        try:
+            msg = client.recv(2048).decode('utf-8')
+            print(f"{msg}\n")
+        except:
+            msg = {
+                "status": 505, "message": "Conexão encerrada", "data": ""
+            }
+            print(f'{msg}\n')
+            client.close()
+            break
+
+
+def enviar_mensagem(client):
+    while True:
+        try:
+            msg = str(input())
+            client.send(f'{msg}'.encode('utf-8'))
+        except:
+            msg = {
+                "status": 504, "message": "Erro ao enviar mensagem", "data": ""
+            }
+
+            return print(f"\n{msg}")
+
+
+if __name__ == "__main__":
+    main()
